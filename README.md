c466cfced79de3ba92ed332b6f5749a05cdb8d1d
Helmut Merz <helmutm@cy55.de>
2020-02-17 17:03:23 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,9 @@
+# cco.storage
+
+This is the Python 2 package for a combined triple and event store.
+The SQL files have moved to storage-common.
+
+Status: planning.
+
+Project website: https://www.cyberconcepts.org.
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.md').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='cco.storage',
+      version=version,
+      description="combined triple and event storage for the cco application platform",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='cyberconcepts.org team',
+      author_email='team@cyberconcepts.org',
+      url='http://www.cyberconcepts.org',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['cco'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'transaction',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
