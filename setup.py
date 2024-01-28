d1dbda5b87aa5133da31fdaa77d43fa706f8c39d
Helmut Merz <helmutm@cy55.de>
2024-01-28 14:18:09 +0100

    fix project (package) settings: now using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,47 +1,4 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-version = '1.0'
+setup()
 
-long_description = (
-    open('README.md').read()
-    + '\n' +
-    'Contributors\n'
-    '============\n'
-    + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
-    + '\n')
-
-setup(name='cco.storage',
-      version=version,
-      description="combined triple and event storage for the cco application platform",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='cyberconcepts.org team',
-      author_email='team@cyberconcepts.org',
-      url='http://www.cyberconcepts.org',
-      license='MIT',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      #namespace_packages=['cco'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'transaction',
-          'psycopg[binary]',
-          'SQLAlchemy',
-          'zope.sqlalchemy',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
